e89a5fab5326a7abdf0d190c77ae6102a8b2acb7
Md. Nuruddin Adnan <nuruddin.adnan1993@gmail.com>
2019-11-13 08:13:21 +0600

    final website version -1

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(".navbar-toggler").click(function() {
+	$(this).toggleClass("navbar-toggler-activated");
+})
+
+// Data Picker Initialization
+$( function() {
+    $( "#datepicker" ).datepicker();
+  } );
